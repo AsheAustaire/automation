2ce0d375443bf7bce4056b52bced528f39f9bce7
Ashe <asheaustaire@gmail.com>
2019-08-24 17:52:39 -0400

    makeUpdates

--- asheboard.js
+++ asheboard.js
@@ -10,11 +10,39 @@ const rl = readline.createInterface({
 
 function main() {
   welcome();
-  options();
+  checkForNamingErrors();
+  // options();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function welcome() {
   console.log('Welcome to Asheboard');
+}
+
+function checkForNamingErrors(){
+  const directoryName = getDirectoriesNames('./workflows')
+  const workflowName = getWorkflowNames('./workflows', directoryName) 
 }
 
 function options() {
@@ -26,26 +54,59 @@ function options() {
       return obj.id === parseInt(line)
     })
     selection = selection[0];
+    // executeTerminalCommand(`/usr/bin/automator ~/.../asheboard/workflows/${selection.directoryName}/${selection.workflow}`)
     rl.close()
-
   })
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function getDirectoriesNames(path) {
   return readdirSync(path, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => `${dirent.name}`)
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => `${dirent.name}`)
+} 
+
+
+function getWorkflowNames(path, directoryName) {
+  directoryName.forEach((directoryName) => {
+    readdirSync(`${path}/${directoryName}`) 
+    //filter based off directory name
+    console.log(readdirSync(`${path}/${directoryName}`))
+  })
 } 
 
 function displayDirectoriesInList(path) {
-  let directoryNameArray = getDirectoriesNames(path)
+  let directoryNameArray = getDirectoriesNames(path);
+  console.log(workflowNameArray)
   return directoryNameArray.map((name, i) => {
-    console.log(`${i}: ${name}`)
+    console.log(`${i}: ${directoryName}`)
     return {
-      'name': name,
-      'id': i
+      'directoryName': directoryName,
+      'id': i,
+      'workflowName': ``
     }
   })
+}
+
+function executeTerminalCommand(command) {
+  EXEC(command)
 }
 
 main();
